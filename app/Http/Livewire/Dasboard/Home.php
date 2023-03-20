96777d883a6c21920f4aa79f559418b1189f5216
ariston <aristonbobi@gmail.com>
2023-03-20 22:16:34 +0800

    update

---
+++ app/Http/Livewire/Dasboard/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dasboard;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.dasboard.home');
+    }
+}
