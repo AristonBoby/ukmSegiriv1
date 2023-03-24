80bcf64f7fb870fed3fc89966f760e9d2f6cc254
AristonBoby <aristonbobi@gmail.com>
2023-03-24 10:28:35 +0800

    update

--- resources/views/livewire/pendaftaran/table.blade.php
+++ resources/views/livewire/pendaftaran/table.blade.php
@@ -1,9 +1,50 @@
 <div class="col-lg-8 col-md-12">
     <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title">Table <b>Pendaftaran</b> </h3>
+            <h3 class="box-title"><i class="fa fa-table"></i> Table <b>Pendaftaran</b> </h3>
         </div>
-        <div class="box-body">
+        <div class="box-body row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="input-group col-lg-3 col-md-3 col-sm-3 pull-right" style="margin-top:20px; margin-bottom:20px;">
+                    <span class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                    </span>
+                    <input type="text" class="form-control" placeholder="dd-mm-yyyy" readonly>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-primary"><i class="fa fa-search"></i> Cari</button>
+                    </span>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <table class="table table-condensed table-stripped table-hover">
+                    <thead>
+                        <tr class="text-center">
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>Tgl Lahir</th>
+                            <th>Umur</th>
+                            <th>Jankel</th>
+                            <th>NIK</th>
+                            <th>KK</th>
+                            <th>Alamat</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Ariston Boby Willy Munte</td>
+                            <td>19-07-1994</td>
+                            <td>28</td>
+                            <td>Laki-laki</td>
+                            <td>1217048787880001</td>
+                            <td>1217048787880001</td>
+                            <td>Jl. Perjuangan Gerilya No.25A RT.104 </td>
+                            <td><button class="btn btn-danger btn-xs"><i class="text-lg fa fa-trash"></i></button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
