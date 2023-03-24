1769136e067e4b62103b5fd5dfc583fccf317e0d
AristonBoby <aristonbobi@gmail.com>
2023-03-24 09:29:52 +0800

    update

--- app/Http/Livewire/Pendaftaran/FormInput.php
+++ app/Http/Livewire/Pendaftaran/FormInput.php
@@ -11,15 +11,31 @@ class FormInput extends Component
     public $jenkel;
     public $kepalaKeluarga;
     public $noKk;
+    public $nik;
+    public $alamat;
 
     protected $rules = ([
-        'nama'      => 'required',
-        'tmpLahir'  => 'required',
+        'nama'              =>  'required||max:60',
+        'tmpLahir'          =>  'required',
+        'tglLahir'          =>  'required',
+        'jenkel'            =>  'required',
+        'kepalaKeluarga'    =>  'required',
+        'noKk'              =>  'required',
+        'nik'               =>  'required',
+        'alamat'            =>  'required',
     ]);
 
-    protected $message = [
-        'nama.require'  => 'Nama wajib diisi'
-    ];
+    public $messages = ([
+        'nama.required'             => 'Nama wajib diisi',
+        'nama.max'                  => 'Maksimal panjang 60 karakter',
+        'tmpLahir.required'         => 'Tempat lahir wajib diisi',
+        'tglLahir.required'         => 'Tempat lahir wajib diisi',
+        'jenkel.required'           => 'Jenis Kelamin wajib diisi',
+        'kepalaKeluarga.required'   => 'Nama kepala Keluarga wajib diisi',
+        'noKk.required'             => 'Nomor Kartu Keluarga',
+        'nik.required'              => 'NIK wajib diisi',
+        'alamat.required'           => 'Alamat wajib diisi',
+    ]);
     public function render()
     {
         return view('livewire.pendaftaran.form-input');
--- resources/views/livewire/pendaftaran/form-input.blade.php
+++ resources/views/livewire/pendaftaran/form-input.blade.php
@@ -1,59 +1,64 @@
 <div class="col-lg-4 col-md-12">
     <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title">Input <b>Data</b> </h3>
+            <h3 class="box-title"><i class="fa fa-edit"></i> Input <b>Data</b> </h3>
         </div>
         <div class="box-body">
             <form wire:submit.prevent="store" class="form-horizontal">
-                <div class="form-group @error('nama')has-error @enderror" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Nama</label>
-                    <div class="col-lg-7">
-                        <input wire:model.defer="nama" type="text" class=" form-control input-md" id="inputError" placeholder="Nama Pasien">
-                        @error('nama')<p class="help-block">{{ $message }}</p> @enderror
+                    <div class="col-lg-7 @error('nama')has-error @enderror">
+                        <input wire:model.defer="nama" type="text" maxlength="60" class=" form-control input-md" id="inputError" placeholder="Nama Pasien">
+                        @error('nama')<p class="help-block mb-4">{{ $message }}</p> @enderror
                     </div>
                 </div>
-            <div class="form-group @error('tmpLahir') has-error @enderror" style="margin-bottom:8px;">
+            <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Tempat Lahir</label>
-                    <div class="col-lg-7">
+                    <div class="col-lg-7 @error('tmpLahir') has-error @enderror">
                         <input wire:model.defer="tmpLahir" type="text" class=" form-control input-md" placeholder="Tempat Lahir">
                         @error('tmpLahir')<p class="help-block">{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Tanggal Lahir</label>
-                    <div class="col-lg-7">
+                    <div class="col-lg-7 @error('tglLahir') has-error @enderror">
                         <input wire.model.defer="tglLahir" type="date" class="form-control input-md" placeholder="Nama Pasien">
+                        @error('tglLahir')<p class="help-block">{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Jenis Kelamin</label>
-                    <div class="col-lg-7">
+                    <div class="col-lg-7 @error('jenkel')has-error @enderror">
                         <select class="form-control input-md" wire:model.defer="jenkel">
                             <option value="">--Pilih Salah Satu--</option>
                             <option value="L">Laki-laki</option>
                             <option value="P">Perempuan</option>
                         </select>
+                        @error('jenkel')<p class="help-block">{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Kepala Keluarga</label>
-                    <div class="col-lg-7">
+                    <div class="col-lg-7 @error('kepalaKeluarga') has-error @enderror">
                         <input wire:model.defer="kepalaKeluarga" type="text" class="input-md form-control" placeholder="Kepala Keluarga">
+                        @error('kepalaKeluarga')<p class="help-block">{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">No. Kartu Keluarga</label>
-                    <div class="col-lg-7">
+                    <div class="col-lg-7 @error('noKk')has-error @enderror">
                         <input wire:model.defer="noKk" type="text" class="input-md form-control" placeholder="Nomor Kartu Keluarga">
+                        @error('noKk')<p class="help-block">{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group " style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">NIK</label>
-                    <div class="col-lg-7">
+                    <div class="col-lg-7 @error('nik') has-error @enderror">
                         <input type="text" class="input-md form-control" placeholder="NIK">
+                        @error('nik')<p class="help-block">{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Provinsi</label>
                     <div class="col-lg-7">
                         <select class="input-md form-control">
@@ -61,7 +66,7 @@
                         </select>
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Kota</label>
                     <div class="col-lg-7">
                         <select class="input-md form-control">
@@ -69,7 +74,7 @@
                         </select>
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Kecamatan</label>
                     <div class="col-lg-7">
                         <select class="input-md form-control">
@@ -77,7 +82,7 @@
                         </select>
                     </div>
                 </div>
-                <div class="form-group" style="margin-bottom:8px;">
+                <div class="form-group" style="margin-bottom:4px;">
                     <label class="col-sm-4 control-label">Kelurahan</label>
                     <div class="col-lg-7">
                         <select class="input-md form-control">
@@ -85,9 +90,17 @@
                         </select>
                     </div>
                 </div>
+                <div class="form-group" style="margin-bottom:4px;">
+                    <label class="col-sm-4 control-label">Alamat</label>
+                    <div class="col-lg-7 @error('alamat') has-error @enderror">
+                        <textarea class="input-md form-control" placeholder="Alamat"></textarea>
+                        @error('alamat')<p class="help-block">{{ $message }}</p> @enderror
+                    </div>
+                </div>
                 <div style="margin-top:40px;">
                     <div>
-                        <button class='pull-right btn btn-primary btn-sm'><i class="fa fa-save"></i> Simpan</button>
+                        <button class='pull-right btn btn-primary btn-flat btn-sm'><i class="fa fa-save"></i> Simpan</button>
+                        <a href="" class='pull-right btn btn-danger btn-flat btn-sm' style="margin-right:4px;"><i class="fa fa-times"></i> Batal</a>
                     </div>
                 </div>
             </form>
